5d88e7df60b697bc9ede53401aba0629891b526b
Ponhrith <ponhrith.nhep@gmail.com>
2023-05-18 23:41:32 +0700

    Testing

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -1,86 +1,61 @@
-const Campground=require('../models/campground');
-const mbxGeocoding=require('@mapbox/mapbox-sdk/services/geocoding');
-const mapBoxToken=process.env.MAPBOX_TOKEN;
-const geocoder=mbxGeocoding({ accessToken: mapBoxToken });
-const {cloudinary}=require('../cloudinary');
+const Campground = require('../models/campground');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const moment = require('moment');
 
-module.exports.index=async(req,res)=>{
-        const campgrounds=await Campground.find({});
-        res.render('campgrounds/index',{campgrounds});
-    }
+module.exports.index = async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+};
 
-module.exports.renderNewForm=(req,res)=>{
-        res.render('campgrounds/new');
-    }
+module.exports.renderNewForm = (req, res) => {
+  res.render('campgrounds/new');
+};
 
-module.exports.createCampground=async (req,res,next)=>{
-    // if(!req.body.campground) throw new ExpressError('campground data Invalid',400); //form is validated but still someone can send request (ex- using postman) then its shoul chek if campground object not present then should not save in database insted throw error whih will got to error handler throug next using catchAsync which we have defined 
-    
-    //instead of writing joi schema and validating here I will define middleware (validateCampgroud) which is passed as an argument  before this funtions runs
-   
-    const geoData= await geocoder.forwardGeocode({
-        query: req.body.campground.location,
-        limit: 1
-      }).send()
+module.exports.createCampground = async (req, res, next) => {
+  const geoData = await geocoder.forwardGeocode({
+    query: req.body.campground.location,
+    limit: 1
+  }).send();
 
-    const campground=new Campground(req.body.campground);
-    campground.geometry=geoData.body.features[0].geometry;
-    campground.images=req.files.map(f=>({url:f.path, filename:f.filename}));  //it will make an array which will contain objects(in which we have url and filename of image)
-    campground.author=req.user._id;
-    campground.createdAt = Date.now(); // Set the createdAt property
-    // console.log(campground);
-    await campground.save();
-    req.flash('success','successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}
+  const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.body.features[0].geometry;
+  campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+  campground.author = req.user._id;
 
+  await campground.save();
+  req.flash('success', 'Successfully made a new campground!');
+  res.redirect(`/campgrounds/${campground._id}`);
+};
 
+module.exports.showCampground = async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+    .populate({
+      path: 'reviews',
+      populate: {
+        path: 'author',
+        select: 'username'
+      }
+    })
+    .populate('author')
+    .lean();
 
-module.exports.showCampground=async(req,res)=>{
-    const campground=await Campground.findById(req.params.id).populate({
-        path:'reviews',  //populating reviews of this campground
-        populate:{
-            path:'author',  //nested population for author of reviews
-            createdAt: true
-        }
-    }).populate('author').lean();  //getting plain object of the campground
-    //console.log(campground);
-    if(!campground){
-        req.flash('error',"can't find that campground");
-        return res.redirect('/campgrounds');
-    }
+  if (!campground) {
+    req.flash('error', "Can't find that campground");
+    return res.redirect('/campgrounds');
+  }
 
-    if(campground){
-        if(campground.createdAt){
-            const daysAgo = Math.floor((Date.now() - campground.createdAt) / (1000 * 60 * 60 * 24));
+  const formattedDate = campground.createdAt ? moment(campground.createdAt).fromNow() : 'N/A';
 
-            const formattedDate = moment(campground.createdAt.toISOString()).fromNow();
-
-            res.render('campgrounds/show',{
-                campground,
-                daysAgo,
-                formattedDate
-            });
-        }else{
-            res.render('campgrounds/show',{
-                campground,
-                daysAgo: "N/A",
-                formattedDate: "N/A"
-            });
-        }
-    }else{
-        res.render('campgrounds/show',{
-            campground: null,
-            daysAgo: "N/A",
-            formattedDate: "N/A"
-        });
-    }
+  res.render('campgrounds/show', {
+    campground,
+    formattedDate
+  });
+};
 
 
-    //console.log(campground)
-    res.render('campgrounds/show',{campground, daysAgo, formattedDate});
-}
+
 
 module.exports.renderEditForm=async(req,res)=>{
     const campground=await Campground.findById(req.params.id);
