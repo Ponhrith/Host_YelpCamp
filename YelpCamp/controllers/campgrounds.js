e9b98631277dd3f7653967da309db59fd8cc99e9
Ponhrith <ponhrith.nhep@gmail.com>
2023-05-19 12:39:33 +0700

    Required Moment

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -3,6 +3,7 @@ const mbxGeocoding=require('@mapbox/mapbox-sdk/services/geocoding');
 const mapBoxToken=process.env.MAPBOX_TOKEN;
 const geocoder=mbxGeocoding({ accessToken: mapBoxToken });
 const {cloudinary}=require('../cloudinary');
+const moment = require('moment');
 
 module.exports.index=async(req,res)=>{
         const campgrounds=await Campground.find({});
